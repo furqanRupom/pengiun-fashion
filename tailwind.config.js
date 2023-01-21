ac91a4ef74f376c90983926aef377a1c3ff9c315
rupom <furqanrupom978@gmail.com>
2023-01-21 23:35:44 +0600

    done

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// module.exports = {
+//     theme: {
+//         container:{
+//             screens: {
+//               'sm': '480px',
+//               // => @media (min-width: 640px) { ... }
+        
+//               'md': '720px',
+//               // => @media (min-width: 768px) { ... }
+        
+//               'lg': '992px',
+//               // => @media (min-width: 1024px) { ... }
+        
+//               'xl': '1000px',
+//               // => @media (min-width: 1280px) { ... }
+        
+//               '2xl': '1200px',
+//               // => @media (min-width: 1536px) { ... }
+//             }
+
+//         }
+//     }
+//   }
